33faf1dc19c17c2eeef7780d56ec397964fddd19
Módli Hunor <mhuni95@gmail.com>
2018-05-26 18:58:56 +0200

    üres example.cpp hozzáadva

---
+++ sources/example.cpp
@@ -0,0 +1,7 @@
+//
+// Created by hunor on 2018.05.26..
+//
+// Az első commitnál a frissen hozzáadott sorokat sor eleji "+" jellel,
+// és zöld háttérszínnel jelöli a github. Amikor ezek a sorok törölve lesznek
+// akkor "-" jellel, és piros háttérszínnel lesznek jelölve!
+
