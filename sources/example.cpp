c766a19a7ee45239bd065a7766538bfc190f0a4a
Módli Hunor <mhuni95@gmail.com>
2018-05-26 19:06:31 +0200

    Hello World hozzáadva az example.cpp-hez!

--- sources/example.cpp
+++ sources/example.cpp
@@ -1,7 +1,10 @@
 //
 // Created by hunor on 2018.05.26..
 //
-// Az első commitnál a frissen hozzáadott sorokat sor eleji "+" jellel,
-// és zöld háttérszínnel jelöli a github. Amikor ezek a sorok törölve lesznek
-// akkor "-" jellel, és piros háttérszínnel lesznek jelölve!
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    cout << "Hello world" << endl;
+}
