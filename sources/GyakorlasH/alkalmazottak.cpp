8aad577859efc09277870746d866d718d45435cf
Módli Hunor <mhuni95@gmail.com>
2018-05-27 18:59:07 +0200

    Filmes feladat megoldva

--- sources/GyakorlasH/alkalmazottak.cpp
+++ sources/GyakorlasH/alkalmazottak.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <map>
 
 using namespace std;
 
@@ -18,160 +17,28 @@ using namespace std;
  *      4) Keresd meg azt az évet amikor a legtöbb dolgozó született.
  */
 
-struct Ido {                /// Csinálunk egy típust az időnek, a könnyebb kezelés érdekében.
-    int honap, nap, ev;
-};
-
-struct Dolgozo {            /// Csinálunk egy Dolgozó osztályt.
-    string nev,
-            telefonszam,
-            email;
-    Ido    szuletesiDatum;
-    string ID;
-};
-
-class Ceg {                         /// Létrehozzuk a Cég osztályt
-    map<string,Dolgozo> nevKulcs;   /// Kell egy névszerinti rendezés -> a név a kulcs.
-    map<Ido,Dolgozo> idoKulcs;      /// Kell egy születési dátum szerinti rendezés -> az idő a kulcs.
+class Ceg {
+    //TODO: Adatszerkezetek definiálása.
 public:
     Ceg() {
-        ifstream f("alkalmazottak.csv");  /// Megnyitjuk a fájlt, megnézzük hogy jó-e.
-        if (!f.good()) {
-            cout << "Hiba a fileban" << endl;
-        }
-        string tmp, mapKulcs;       /// A tmp-be megy az első felesleges sor, a mapKulcs-ba a név.
-        getline(f,tmp);             /// Eldobjuk az első sort.
-        char kuka;                  /// Ezzel fogjuk eldobni a ','-ket, és a dátumot elválasztó '/'-t.
-        Dolgozo dolgTmp;            /// Valamint csinálunk egy Dolgozót, és egy Időt, amikobe olvasunk.
-        Ido idoTmp;
-        while (getline(f,mapKulcs,',')) {       /// Beolvasunk a két névbe.
-            dolgTmp.nev = mapKulcs;
-            getline(f,dolgTmp.telefonszam,','); /// Majd a telefonszámba
-            getline(f,dolgTmp.email,',');       /// És e-mailbe.
-
-            /**     Itt kicsit megint trükkös, kell olvasni egyszer a hónapba,
-                    Majd jön egy '\' karakter, amit berakuknk a kukába.
-                    Ez ismételjük a többi adattaggal, majd a végén beolvassuk a ','-t. */
-            f >> idoTmp.honap >> kuka >> idoTmp.nap >> kuka >> idoTmp.ev >> kuka;
-
-            dolgTmp.szuletesiDatum = idoTmp;    /// A Dolgozó változónak értékül adjuk a most beolvasott Ido-t.
-            getline(f,dolgTmp.ID);              /// Majd végül beolvasunk az ID-be.
-            nevKulcs[mapKulcs] = dolgTmp;       /// És hozzáadjuk a két map-hez az elemeket.
-            /** FONTOS!
-             Ahoz, hogy Ido tíőusú változót kulcsul adjunk egy mapnek definiálnuk kell két
-             Ido típusú változó között a '<' operátort. (Implementáció lent) */
-            idoKulcs[idoTmp] = dolgTmp;
-        }
-        f.close();      /// Legvégül bezárjuk a fájlt.
+        //TODO: Konstruktor megírása.
     }
 
-    void elsoFeladat() {            /// Az adatszerkezetünknek hála, mostmár csak végig kell iterálni
-        for (auto a : nevKulcs) {   /// A név szerint rendezett map-en, s kiiratni konzolra.
-            cout << a.second.nev << " tel.: " << a.second.telefonszam
-                 << " e-mail: " << a.second.email << endl;
-        }
+    void elsoFeladat() {
+        //TODO
     }
-
-    void masodikFeladat() {         /// Ugyanolyan könnyű a feladat mint az előbb, csak itt fájlba kell írni.
-        ofstream oFile("idoSorrend.txt");   /// Megnyitunk egy fájlt.
-        for (auto a:idoKulcs) {             /// Majd végigiterálunk a fájlon, és kiiratjuk az adatokat.
-            oFile << a.second.szuletesiDatum.ev << " " << a.second.szuletesiDatum.honap
-                  << " " << a.second.szuletesiDatum.nap << " : " << a.second.nev <<  endl;
-        }
-        oFile.close();      /// Végül bezárjuk a fájlt.
+    void masodikFeladat() {
+        //TODO
     }
-
     void harmadikFeladat() {
-        Dolgozo d;              /// Csinálunk egy Dolgozót, és egy Időt, mint abeolvasásnál.
-        Ido i;
-        cout << "Add meg a nevet:" << endl;     /// Majd bekérjük az adatokat.
-        getline(cin,d.nev);
-        cout << "Add meg a telefonszamot:" << endl;
-        getline(cin,d.telefonszam);
-        cout << "Add meg az email cimet:" << endl;
-        getline(cin,d.email);
-        cout << "Add meg a szuletesi datumot:" << endl << "ev: ";
-        cin >> i.ev;
-        cin.ignore();           /// Az ignore() kell, mivel a cin >> beolvasásnál az enter miatt egy newline
-        /// karakter ('\n') ottmarad  az input buffer-ben, ezt így lehet eltüntetni.
-        cout << "honap: ";
-        cin >> i.honap;
-        cin.ignore();
-        cout << "nap: ";
-        cin >> i.nap;
-        cin.ignore();
-        d.szuletesiDatum = i;
-        cout << "Add meg az azonositot:" << endl;
-        getline(cin,d.ID);
-        nevKulcs[d.nev] = d;    /// Az adatok bekérése utén hozzáadjuk a map-ekhez.
-        idoKulcs[i] = d;
+        //TODO
     }
 
     void negyedikFeladat() {
-        /** A feladat könnyű megoldásához csinálunk egy mapet, ahol a
-            kulcs az év, az érték pedig, hogy hányszor fordul elő. */
-        map<int,int> evekhezElofordulas;
-        for (auto a : idoKulcs) {        /// Végimegyünk az idő kulcsú mapen
-            if (evekhezElofordulas.count(a.first.ev)) {     /// Ha már hozzá van adva az adott év
-                evekhezElofordulas[a.first.ev]++;           /// akkor hozzáadunk az értékhez.
-            } else {
-                evekhezElofordulas[a.first.ev] = 1;         /// Ha nincs akkor létrhozzuk 1-el.
-            }
-        }
-
-        /// Ezek után kell egy maximumkeresés az értékek között.
-        map<int,int>::iterator it = evekhezElofordulas.begin();
-        int ev = it->first;
-        int legtobb = it->second;
-        for (auto a : evekhezElofordulas) {
-            if (a.second > legtobb) {
-                ev = a.first;
-                legtobb = a.second;
-            }
-        }
-        cout << "Ebben az evben szuletett a legtobb dolgozo: " << ev << " (" << legtobb << ")" << endl;
-
+        //TODO
     }
 };
-/** A '<' operátort meg kell írni két Ido típusra, hogy mapnek
-    kulcsként lehessen adni. */
-bool operator < (const Ido &i1,const  Ido &i2) {
-    if (i1.ev == i2.ev) {            /// Először megnézzük az év szerinti egyezést
-        if (i1.honap == i2.honap) {  /// Majd hónap szerintit is.
-            return i1.nap < i2.nap;  /// Ha mindkettő egyezik akkor a nap dönt.
-        }
-        return i1.honap < i2.honap;  /// Ha csak az év akkor a hónap.
-    }
-    return i1.ev < i2.ev;            /// Ha nem egyezik az év akkor az év.
-}
-
-
 int main() {
     Ceg ceg;
-    bool fut = true;    /// Majd írunk egy kezdetleges user interface-t.
-    int userInput;
-    while (fut) {
-        cin >> userInput;
-        cin.ignore();
-        switch(userInput) {
-            case 1:
-                ceg.elsoFeladat();
-                break;
-            case 2:
-                ceg.masodikFeladat();
-                break;
-            case 3:
-                ceg.harmadikFeladat();
-                break;
-            case 4:
-                ceg.negyedikFeladat();
-                break;
-            case 0:
-                fut = false;
-                break;
-            default:
-                cout << "Hibas input, probalj masikat!" << endl;
-                break;
-        }
-    }
+    //TODO: user interface megírása.
 }
--- sources/GyakorlasH/filmek.cpp
+++ sources/GyakorlasH/filmek.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <set>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
+
+int myAtoi(string s) {
+    stringstream ss;
+    int i;
+    ss << s;
+    ss >> i;
+    return i;
+}
+
 
 /**	Feladat: olvasd be filmek adatait a filmek.csv fájlból megfelelő adatszerkezetbe, majd végezd el a következő feladatokat:
  *		1) Irasd ki a leghosszabb játékidejű filmet.
@@ -10,25 +25,160 @@ using namespace std;
  * 	A program világosan kommunikáljon a felhasználóval, és a 0 inputra ki lehessen lépni belőle.
  */
 
-class Filmek {
-    //TODO: adatszerkezet(ek) kiválasztása
+struct Film {
+    string nev;
+    string kategoria;
+    int jatekido;
+    int ev;
+    string szinesz;
+    float imdb;
+};
+
+bool operator < (Film f1, Film f2) {
+    if (f1.jatekido == f2.jatekido) {
+        return f1.nev < f2.nev;
+    }
+    return f1.jatekido > f2.jatekido;
+}
+
+ostream &operator << (ostream &of, Film f) {
+    cout << "Nev: " << f.nev << ", Kategoria: " << f.kategoria
+         << ", Jatekido: " << f.jatekido << ", Ev: " << f.ev
+         << ", Szinesz " << f.szinesz << ", Imdb:" << f.imdb;
+    return of;
+}
+
+bool imdbAlapjan(Film f1, Film f2) { // vector max_element függvényáhez kell hogy mi alapján keressen maxot!
+    return f1.imdb < f2.imdb;
+}
+
+class Feladat {
+    set<Film> filmekSet;
+    map<string, vector<Film>> filmekKategoriahoz;
+
 public:
-    Filmek() {
-        //TODO
+    Feladat() {
+        ifstream f("filmek.csv");
+        if (!f.good()) {
+            cout << "Nem sikerült megnyitni a fájlt!" << endl;
+        } else {
+//            string s = "12";
+//            int a = atoi(s.c_str());
+            string tmp;
+
+            getline(f,tmp);
+
+            Film film;
+            string kategoria; // MAP-nek a kategória kulcsa stringként
+
+//            cout << film.nev << ", " << film.ev << ", " << film.imdb;
+            while(getline(f,film.nev,',')) { // Beolvastuk a sorból a címet az első vesszőig
+                getline(f,tmp,',');
+                film.kategoria = tmp;
+                kategoria = tmp;
+                getline(f,tmp,',');
+                film.jatekido = atoi(tmp.c_str());
+//                film.jatekido = myAtoi(tmp); // alternatíva ha az atoi kudarcot vall
+                getline(f,tmp,',');
+                film.ev = atoi(tmp.c_str());
+                getline(f,film.szinesz,',');
+                getline(f,tmp);
+                film.imdb = atof(tmp.c_str());
+
+                filmekSet.insert(film);
+
+                if (filmekKategoriahoz.count(kategoria)) { // Ha már létezik ilyen kulcsú elem a map-ben
+                    //, azaz van már ilyen kategória a string kulcsok között, akkor csak hozzáadjuk a vektorhoz a filmet.
+                    filmekKategoriahoz[kategoria].push_back(film);
+                } else { // különben létre kell hozni egy vectort amiben az első film van az adott kategóriához.
+                    vector<Film> v;
+                    v.push_back(film);
+                    filmekKategoriahoz[kategoria] = v;
+                }
+
+            }
+            f.close();
+        }
     }
 
     void elsoFeladat() {
-        //TODO
+//        cout << "Elso feladat ide" << endl;
+//        cout << "Map mérete: " << filmekKategoriahoz.size() << endl;
+//        cout << "Set mérete: " << filmekSet.size() << endl;
+//        cout << "Set tartalma: " << endl;
+//
+//        for (auto i : filmekSet) {
+//            cout << i << endl;
+//        }
+//
+//        cout << "Map tartalma" << endl;
+//
+//        for (auto i : filmekKategoriahoz) {
+//            cout << "Kategoria: " << i.first << endl;
+//            for (auto j : i.second) {
+//                cout << j << endl;
+//            }
+//        }
+        cout << "Leghosszabb játékidejű film: " << endl;
+        cout << *filmekSet.begin() << endl;
     }
+
     void masodikFeladat() {
-        //TODO
+        cout << "A Filmek Kategóriák szerint: " << endl;
+
+        for (auto i : filmekKategoriahoz) {
+            cout << "Kategoria: " << i.first << endl << "------------------" << endl;
+            for (auto j : i.second) {
+                cout << j.nev << ", " << j.imdb << endl;
+            }
+            cout << endl << endl;
+        }
+
     }
     void harmadikFeladat() {
-        //TODO
+        string tmp;
+        cout << "Írjon be egy kategóriát!" << endl;
+        cin >> tmp;
+
+        if (filmekKategoriahoz.count(tmp)) { // ha létezik ilyen kategória akkor maxkeresés
+
+            vector<Film> v = filmekKategoriahoz.at(tmp);
+
+            // algorithm könyvtár része. működés:
+            // param1: iterátor a tömb elejére
+            // param2 iterátor a tömb végére.
+            // eredmény: iterátor a max elemre. (*-gal dereferálni kell!!!)
+            Film max = *max_element(v.begin(), v.end(), imdbAlapjan);
+            cout << max << endl;
+        } else { // ha nem létezik ilyen akkor kiírjuk
+            cout << "Nincs ilyen kategória!";
+        }
     }
 };
 
 int main() {
-    Filmek osszesFilm;
-    //TODO: user interface megírása.
+    Feladat feladat;
+    bool fut = true;
+    int bemenet;
+    while (fut) {
+        cin >> bemenet;
+        cin.ignore(); // néha kell, néha nem :D (Code::Blocks egy fos ^__^ )
+        switch (bemenet){
+            case 0:
+                fut = false;
+                break;
+            case 1:
+                feladat.elsoFeladat();
+                break;
+            case 2:
+                feladat.masodikFeladat();
+                break;
+            case 3:
+                feladat.harmadikFeladat();
+                break;
+            default:
+                cout << "Nem megfelelő bemenet. Próbáld újra!" << endl;
+                break;
+        }
+    }
 }
