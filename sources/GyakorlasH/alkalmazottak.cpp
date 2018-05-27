e5330653327001aae312faafb41623ce8dbba9f2
Módli Hunor <mhuni95@gmail.com>
2018-05-27 14:38:39 +0200

    hazudtam, igazából ez az üres alkalmazottal.cpp :D

--- sources/GyakorlasH/alkalmazottak.cpp
+++ sources/GyakorlasH/alkalmazottak.cpp
@@ -3,18 +3,25 @@
 
 using namespace std;
 
-/**	Feladat: olvasd be filmek adatait a filmek.csv fájlból megfelelő adatszerkezetbe, majd végezd el a következő feladatokat:
- *		1) Irasd ki a leghosszabb játékidejű filmet.
- *		2) Irasd ki a filmeket Kategória szerint rendeztve, valamint látszódjon az imdb értékelésük is.
- *		3) Ha felhasználó beír egy Kategóriát, akkor adja meg, hogy melyik a legjobb film a kategóriában (imdb alapján).
- * 	A program világosan kommunikáljon a felhasználóval, és a 0 inputra ki lehessen lépni belőle.
+/**     Gyakoroljunk még egy kis fájlkezelést! Adott egy "Raw Data.txt" nevű file,
+ *      mely egy cég 100 alkalmazottjának adatait tartalmazza a következőképp:
+ *
+ *      Név,Telefonszám,Email cím,Születési Idő,Azonosítószám
+ *  Pl: Michael Cortez,(0141) 554 7341,02/22/1979,placerat@sed.edu,1663061503199
+ *      Figyelj rá, hogy a telefonszám többféle formátumban is megadható!
+ *      Olvasd be a fileból az adatokat, egy (vagy több) adatszerkezetbe, majd végezd el a feladatokat:
+ *
+ *      1) Irasd ki a cég dolgozóit abc sorrendben, az elérhetőségükkel együtt (tel, email).
+ *      2) Irasd ki a cég dolgozóit születési sorrendben egy fileba.
+ *      3) Tudj hozzáadni az adatbázishoz. Ellenőrizd le az 1., és 2. feladatokkal.
+ *      4) Keresd meg azt az évet amikor a legtöbb dolgozó született.
  */
 
-class Filmek {
-    //TODO: adatszerkezet(ek) kiválasztása
+class Ceg {
+    //TODO: Adatszerkezetek definiálása.
 public:
-    Filmek() {
-        //TODO
+    Ceg() {
+        //TODO: Konstruktor megírása.
     }
 
     void elsoFeladat() {
@@ -26,10 +33,12 @@ public:
     void harmadikFeladat() {
         //TODO
     }
+
+    void negyedikFeladat() {
+        //TODO
+    }
 };
-
-
 int main() {
-    Filmek osszesFilm;
+    Ceg ceg;
     //TODO: user interface megírása.
 }
